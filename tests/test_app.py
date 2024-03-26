122f0ebcecc637a0b9de914c6fe5c8954ffd0707
srivatsan1303 <sj796@njit.edu>
2024-03-25 21:18:25 -0400

    test cases created for load,clear and delete commands

--- tests/test_app.py
+++ tests/test_app.py
@@ -85,3 +85,45 @@ def test_app_start_divide(capfd, monkeypatch):
 
     captured = capfd.readouterr()
     assert "5" in captured.out, "The division command did not produce the expected output."
+
+def test_app_start_load(capfd, monkeypatch):
+    """Test how the REPL handles an fetch command before exiting."""
+    # Simulate user entering an unknown command followed by 'exit'
+    inputs = iter(['load', 'exit'])
+    monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+
+    app = App()
+
+    with pytest.raises(SystemExit) :
+        app.start()
+
+    captured = capfd.readouterr()
+    assert "Operation history of calculator:" in captured.out
+
+def test_app_start_delete(capfd, monkeypatch):
+    """Test how the REPL handles an delete command before exiting."""
+    # Simulate user entering an unknown command followed by 'exit'
+    inputs = iter(['delete', 5, 'exit'])
+    monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+
+    app = App()
+
+    with pytest.raises(SystemExit) :
+        app.start()
+
+    captured = capfd.readouterr()
+    assert "Operation history of calculator after deletion:" in captured.out
+
+def test_app_start_clear(capfd, monkeypatch):
+    """Test how the REPL handles an clear command before exiting."""
+    # Simulate user entering an unknown command followed by 'exit'
+    inputs = iter(['clear', 'exit'])
+    monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+
+    app = App()
+
+    with pytest.raises(SystemExit) :
+        app.start()
+
+    captured = capfd.readouterr()
+    assert "History has been cleared!" in captured.out
